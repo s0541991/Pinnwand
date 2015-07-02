a2a1589663380586ac2257e578b5cdba1dcb02c4
anhnguyenhoang <s0541991@htw-berlin.de>
2015-07-02 13:12:35 +0200

    Layout for Login and Register XML done. Mainactivity changes to Login activity at the start of the app

--- Pinnwand/src/com/example/pinnwand/MainActivity.java
+++ Pinnwand/src/com/example/pinnwand/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pinnwand;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        startActivity(new Intent(MainActivity.this, Login.class));
     }
 
 }
