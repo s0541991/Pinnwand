ed7ec40845ef364286600874cbdbdea824366e28
anhnguyenhoang <s0541991@htw-berlin.de>
2015-07-03 14:49:08 +0200

    "Mein Konto" und "User" activity

--- Pinnwand/src/com/example/pinnwand/MeinKonto.java
+++ Pinnwand/src/com/example/pinnwand/MeinKonto.java
@@ -4,26 +4,28 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.Button;
 
-public class MeineThreads extends Activity {
-	
-	Button kontext_menu;
+public class MeinKonto extends Activity {
 
+	Button kontext_menu;
+	Button change;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_meine_threads);
+		setContentView(R.layout.activity_mein_konto);
 		kontext_menu = (Button) findViewById(R.id.kontext_menu);
+		change = (Button) findViewById(R.id.change);
 		
 		registerForContextMenu(kontext_menu);
-	}	
-
+	}
+	
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
@@ -42,16 +44,17 @@ public class MeineThreads extends Activity {
 			case R.id.neu:
 				break;
 			case R.id.mein_konto:
-					startActivity(new Intent(MeineThreads.this, MeinKonto.class));
+					startActivity(new Intent(MeinKonto.this, MeinKonto.class));
 				break;
 			case R.id.meine_threads:
 				break;
 			case R.id.aktualisieren:
 				break;
 			case R.id.logout:
-					startActivity(new Intent(MeineThreads.this, Login.class));
+					startActivity(new Intent(MeinKonto.this, Login.class));
 				break;
 		}
 		return super.onContextItemSelected(item);
 	}
+	
 }
--- Pinnwand/src/com/example/pinnwand/Mitglied.java
+++ Pinnwand/src/com/example/pinnwand/Mitglied.java
@@ -4,25 +4,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.Button;
 
-public class MeineThreads extends Activity {
-	
-	Button kontext_menu;
+public class Mitglied extends Activity {
 
+	Button kontext_menu;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_meine_threads);
+		setContentView(R.layout.activity_mitglied);
+		
 		kontext_menu = (Button) findViewById(R.id.kontext_menu);
 		
 		registerForContextMenu(kontext_menu);
-	}	
+	}
 
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
@@ -42,14 +42,14 @@ public class MeineThreads extends Activity {
 			case R.id.neu:
 				break;
 			case R.id.mein_konto:
-					startActivity(new Intent(MeineThreads.this, MeinKonto.class));
+					startActivity(new Intent(Mitglied.this, MeinKonto.class));
 				break;
 			case R.id.meine_threads:
 				break;
 			case R.id.aktualisieren:
 				break;
 			case R.id.logout:
-					startActivity(new Intent(MeineThreads.this, Login.class));
+					startActivity(new Intent(Mitglied.this, Login.class));
 				break;
 		}
 		return super.onContextItemSelected(item);
